f38006e7aa87fe2a6e0b3799009ed2413d896361
Dan Graham <dan@saucelabs.com>
2016-11-28 17:51:07 -0800

    Added desired caps form
    Takes the desired caps from appium-base-driver and then renders the form based on the definition

--- app/components/Session.js
+++ app/components/Session.js
@@ -1,5 +1,17 @@
 import React, { Component, PropTypes } from 'react';
-import styles from './ServerMonitor.css';
+import styles from './Session.css';
+
+const { desiredCapabilityConstraints } = require('appium-base-driver/build/lib/basedriver/desired-caps');
+
+function unCamelCase (str) {
+  return str
+    // insert a space between lower & upper
+    .replace(/([a-z])([A-Z])/g, '$1 $2')
+    // space before last upper in a sequence followed by lower
+    .replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3')
+    // uppercase the first character
+    .replace(/^./, function (str) { return str.toUpperCase(); });
+}
 
 export default class Session extends Component {
   static propTypes = {
@@ -15,7 +27,34 @@ export default class Session extends Component {
   render () {
     return (
       <div className={styles.container}>
-        <h1>Session!</h1>
+        <h2>Start New Session</h2>
+        <form>
+        <table>
+        {Object.keys(desiredCapabilityConstraints).map((key) => {
+          let cap = desiredCapabilityConstraints[key];
+          let form;
+          if (cap.inclusionCaseInsensitive) {
+            form=<select>
+              { cap.inclusionCaseInsensitive.map((name) => <option name={name}>{name}</option>) }
+            </select>;
+          } else if (cap.isBoolean) {
+            form = <input id={key} type='checkbox' name={key} />;
+          } else if (cap.isNumber) {
+            form = <input id={key} type='number' name={key} />;
+          } else {
+            form = <input id={key} type='text' name={key} />;
+          } 
+
+          return <tr> 
+            <td>
+                <label for={key}>{unCamelCase(key)}</label>
+            </td>
+            <td>{form}</td>
+          </tr>;
+
+        })}
+        </table>
+        </form>
       </div>
     );
   }
