ada9191d9240b687eff756f27d25c6cafda06b4d
Dan Graham <dan@saucelabs.com>
2016-11-28 16:40:52 -0800

    Stubbed in session component
    Removed session.html and instead just opens index.html but opens it at the newly created /sessions route

--- app/routes.js
+++ app/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import StartServerPage from './containers/StartServerPage';
 import ServerMonitorPage from './containers/ServerMonitorPage';
+import SessionPage from './containers/SessionPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={StartServerPage} />
     <Route path="monitor" component={ServerMonitorPage} />
+    <Route path="session" component={SessionPage} />
   </Route>
 );
