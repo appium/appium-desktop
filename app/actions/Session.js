4d63a71187dcbc969e047b786e663d24521bf9b7
Dan Graham <dan@saucelabs.com>
2016-11-30 15:15:35 -0800

    Notify session window when new session ready
    Also notifies when session failed. Right now it just alerts that it succeeded or failed

--- app/actions/Session.js
+++ app/actions/Session.js
@@ -10,6 +10,16 @@ export function newSessionRequested (desiredCapabilities) {
 export function newSession (desiredCapabilities) {
   return (dispatch) => {
     dispatch(newSessionRequested(desiredCapabilities));
+
+    ipcRenderer.once('appium-new-session-ready', (event, message) => {
+      alert('Successfully started session');
+    });
+
+    ipcRenderer.once('appium-new-session-failed', (event, message) => {
+      ipcRenderer.removeAllListeners('appium-log-line');
+      alert('Error starting session');
+    });
+
     ipcRenderer.send('appium-create-new-session', desiredCapabilities, () => {
       dispatch(push('/inspector'));
     });
