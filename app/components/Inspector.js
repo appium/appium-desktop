9680dbd47b20c9c9011720f01921d65125ac9d75
Dan Graham <dan@saucelabs.com>
2016-12-12 15:17:35 -0800

    Removed unused code in Inspector component

--- app/components/Inspector.js
+++ app/components/Inspector.js
@@ -2,22 +2,6 @@ import React, { Component, PropTypes } from 'react';
 import styles from './Session.css';
 
 export default class Inspector extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      
-    };
-  }
-
-  static propTypes = {
-    newSession: PropTypes.func,
-  }
-
-  componentWillUpdate () {
-  }
-
-  componentDidUpdate () {
-  }
 
   render () {
     return (
