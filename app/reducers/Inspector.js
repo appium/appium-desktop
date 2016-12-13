3d543dac37f786241fe7ba41ff1c6a55400048af
Dan Graham <dan@saucelabs.com>
2016-12-12 19:15:18 -0800

    Removed Immutable.js code

--- app/reducers/Inspector.js
+++ app/reducers/Inspector.js
@@ -1,4 +1,4 @@
-import Immutable from 'immutable';
+import _ from 'lodash';
 
 import { SET_SOURCE_AND_SCREENSHOT, QUIT_SESSION_REQUESTED, QUIT_SESSION_DONE, SELECT_ELEMENT, UNSELECT_ELEMENT,
   METHOD_CALL_REQUESTED, METHOD_CALL_DONE, SET_INPUT_VALUE, SET_EXPANDED_XPATHS } from '../actions/Inspector';
@@ -11,52 +11,54 @@ const INITIAL_STATE = {
 export default function inspector (state=INITIAL_STATE, action) {
   switch (action.type) {
     case SET_SOURCE_AND_SCREENSHOT:
-      return Immutable.fromJS(state)
-        .set('source', action.source)
-        .set('screenshot', action.screenshot)
-        .toJS();
+      return {
+        ...state,
+        source: action.source,
+        screenshot: action.screenshot,
+      };
 
     case QUIT_SESSION_REQUESTED:
-      return Immutable.fromJS(state)
-        .set('isQuittingSession', true)
-        .toJS();
-
-    case QUIT_SESSION_DONE:
-      return Immutable.fromJS(state)
-        .delete('isQuittingSession')
-        .toJS();
+      return {
+        ...state,
+        isQuittingSession: true,
+      };
       
+    case QUIT_SESSION_DONE:
+      return _.omit(state, 'isQuittingSession');
+
     case SELECT_ELEMENT:
-      return Immutable.fromJS(state)
-        .setIn(['selectedNode', 'xpath'], action.xpath)
-        .setIn(['selectedNode', 'attributes'], action.attributes)
-        .setIn(['selectedNode', 'tagName'], action.tagName)
-        .toJS();
+      return {
+        ...state,
+        selectedNode: {
+          xpath: action.xpath,
+          attributes: action.attributes,
+          tagName: action.tagName,
+        }
+      };
 
     case UNSELECT_ELEMENT:
-      return Immutable.fromJS(state)
-      .delete('selectedNode')
-      .toJS();
+      return _.omit(state, 'selectedNode');
 
     case METHOD_CALL_REQUESTED:
-      return Immutable.fromJS(state)
-        .set('methodCallRequested', true)
-        .toJS();
+      return {
+        ...state,
+        methodCallRequested: true,
+      };
 
     case METHOD_CALL_DONE:
-      return Immutable.fromJS(state)
-        .delete('methodCallRequested')
-        .toJS();
+      return _.omit(state, 'methodCallRequested');
 
     case SET_INPUT_VALUE:
-      return Immutable.fromJS(state)
-        .set(action.name, action.value)
-        .toJS();
+      return {
+        ...state,
+        [action.name]: action.value,
+      };
 
     case SET_EXPANDED_XPATHS:
-      return Immutable.fromJS(state)
-        .set('expandedXPaths', action.xpaths)
-        .toJS();
+      return {
+        ...state,
+        expandedXPaths: action.xpaths,
+      };
 
     default:
       return {...state};
