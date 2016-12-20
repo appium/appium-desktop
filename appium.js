2fd44031b5e6620732b557b537c03c2a3575fb54
Dan Graham <dan@saucelabs.com>
2016-12-20 13:45:12 -0800

    Pull request fixes

--- appium.js
+++ appium.js
@@ -123,7 +123,7 @@ function connectCreateNewSessionWindow (win) {
       sessionWin = null;
     });
 
-    // If it's dev, include devTools and 'inspect element' context menu option
+    // If its dev, include devTools and 'inspect element' context menu option
     if (isDev) {
       sessionWin.openDevTools();
       sessionWin.webContents.on('context-menu', (e, props) => {
