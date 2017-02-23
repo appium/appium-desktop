94cf00f46b858f4d41429df9346b4040e2acf103
Dan Graham <dan@saucelabs.com>
2017-02-22 19:28:11 -0800

    Can request main process to download
    Added button that dispatches an action that tells the main process to start downloading

--- app/renderer/actions/Updater.js
+++ app/renderer/actions/Updater.js
@@ -1,7 +1,18 @@
+import { ipcRenderer } from 'electron';
+
 export const UPDATE_INFO = 'UPDATE_INFO';
+export const DOWNLOAD_UPDATE_REQUESTED = 'DOWNLOAD_UPDATE_REQUESTED';
+
 
 export function foundAvailableUpdate (updateInfo) {
   return (dispatch) => {
     dispatch({type: UPDATE_INFO, updateInfo});
+  };
+}
+
+export function requestUpdateDownload () {
+  return (dispatch) => {
+    ipcRenderer.send('update-download-request');
+    dispatch({type: DOWNLOAD_UPDATE_REQUESTED});
   };
 }
--- app/renderer/components/Updater.js
+++ app/renderer/components/Updater.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 //import NewSessionForm from './Session/NewSessionForm';
 //import SavedSessions from './Session/SavedSessions';
-//import { Tabs, Form, Input, Button, Spin, Card } from 'antd';
+import { Button } from 'antd';
 //import { UpdaterTypes } from '../actions/Updater';
 //import UpdaterStyles from './Updater.css';
 import { ipcRenderer } from 'electron';
@@ -25,12 +25,17 @@ export default class Updater extends Component {
   }
 
   render () {
-    const { updateInfo } = this.props;
+    const { updateInfo, requestUpdateDownload, isDownloading } = this.props;
+    if (!updateInfo) return null;
+
     const { releaseDate, releaseNotes, version } = updateInfo;
     return <div>
         <h3>A new version of Appium Desktop is ready: <span>{version}</span> released <span>{releaseDate}</span></h3>
         <h4>Release Notes</h4>
         <p>{releaseNotes}</p>
+        <span>{isDownloading}</span>
+        <Button onClick={requestUpdateDownload} disabled={isDownloading}>{isDownloading ? 'Downloading' : 'Download Update Now'}</Button>
+        {!isDownloading && <Button>Ask Me Later</Button>}
     </div>;
   }
 }
