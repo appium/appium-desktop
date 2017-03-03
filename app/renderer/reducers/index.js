4b142fd6e7b200bf0b13e368dddf686ea397c381
Jonathan Lipps <jlipps@gmail.com>
2017-03-02 21:08:19 -0800

    create a shared settings object so we don't clobber it in different files (fix #57)

--- app/renderer/reducers/index.js
+++ app/renderer/reducers/index.js
@@ -5,20 +5,6 @@ import serverMonitor from './ServerMonitor';
 import session from './Session';
 import inspector from './Inspector';
 import updater from './Updater';
-import settings from 'electron-settings';
-import { PRESETS } from '../actions/StartServer';
-import { SAVED_SESSIONS, SERVER_ARGS, SESSION_SERVER_PARAMS,
-         SESSION_SERVER_TYPE } from '../actions/Session';
-
-// set default persistent settings
-// do it here because settings are kind of like state!
-settings.defaults({
-  [PRESETS]: {},
-  [SAVED_SESSIONS]: [],
-  [SERVER_ARGS]: null,
-  [SESSION_SERVER_PARAMS]: null,
-  [SESSION_SERVER_TYPE]: null,
-});
 
 // create our root reducer
 const rootReducer = combineReducers({
