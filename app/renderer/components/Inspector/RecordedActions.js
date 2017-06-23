9883ed47b0a7e117923149970d14191923b57b8d
Jonathan Lipps <jlipps@gmail.com>
2017-06-23 10:10:54 -0700

    add syntax highlighting for framework code

--- app/renderer/components/Inspector/RecordedActions.js
+++ app/renderer/components/Inspector/RecordedActions.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import InspectorStyles from './Inspector.css';
 import frameworks from '../../client-frameworks';
+import { highlight } from 'highlight.js';
 
 export default class RecordedActions extends Component {
   render () {
@@ -9,6 +10,8 @@ export default class RecordedActions extends Component {
     // TODO add server connection info to this constructor call
     let framework = new frameworks[actionFramework]("foo", "bar", "baz");
     framework.actions = recordedActions;
+    const rawCode = framework.getCodeString();
+    const highlightedCode = highlight(framework.language, rawCode, true).value;
     // TODO use framework.language to set up syntax highlighting
     return <div>
       {!recordedActions.length &&
@@ -17,10 +20,10 @@ export default class RecordedActions extends Component {
        </div>
       }
       {!!recordedActions.length &&
-       <div className={InspectorStyles['recorded-code']}>
-          {framework.getCodeString()}
-       </div>
+       <div className={InspectorStyles['recorded-code']}
+        dangerouslySetInnerHTML={{__html: highlightedCode}} />
       }
     </div>;
   }
 }
+
