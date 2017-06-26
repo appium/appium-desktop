02c34ec648c50e08b205fda2aad700e14baa7ef1
Jonathan Lipps <jlipps@gmail.com>
2017-06-26 10:59:56 -0700

    add ruby framework

--- app/renderer/lib/client-frameworks/index.js
+++ app/renderer/lib/client-frameworks/index.js
@@ -2,12 +2,14 @@ import JsWdFramework from './js-wd';
 import JsWdIoFramework from './js-wdio';
 import JavaFramework from './java';
 import PythonFramework from './python';
+import RubyFramework from './ruby';
 
 const frameworks = {
   jsWd: JsWdFramework,
   jsWdIo: JsWdIoFramework,
   java: JavaFramework,
   python: PythonFramework,
+  ruby: RubyFramework,
 };
 
 export default frameworks;
---
+++ app/renderer/lib/client-frameworks/ruby.js
@@ -0,0 +1,62 @@
+import Framework from './framework';
+
+class RubyFramework extends Framework {
+
+  get language () {
+    return "ruby";
+  }
+
+  wrapWithBoilerplate (code) {
+    let capStr = Object.keys(this.caps).map((k) => {
+      return `caps[${JSON.stringify(k)}] = ${JSON.stringify(this.caps[k])}`;
+    }).join("\n");
+    return `# This sample code uses the Appium ruby client
+# gem install appium_lib
+# Then you can paste this into a file and simply run with Ruby
+
+require 'rubygems'
+require 'appium_lib'
+
+caps = {}
+${capStr}
+opts = {
+    sauce_username: nil,
+    server_url: "http://${this.host}:${this.port}/wd/hub"
+}
+driver = Appium::Driver.new({caps: caps, appium_lib: opts}).start_driver
+
+${code}
+driver.quit`;
+  }
+
+  codeFor_findAndAssign (strategy, locator, localVar) {
+    let suffixMap = {
+      xpath: ":xpath",
+      // TODO add other locator strategies
+    };
+    if (!suffixMap[strategy]) {
+      throw new Error(`Strategy ${strategy} can't be code-gened`);
+    }
+    return `${localVar} = driver.find_element(${suffixMap[strategy]}, ${JSON.stringify(locator)})`;
+  }
+
+  codeFor_click () {
+    return `${this.lastAssignedVar}.click`;
+  }
+
+  codeFor_clear () {
+    return `${this.lastAssignedVar}.clear`;
+  }
+
+  codeFor_sendKeys (text) {
+    return `${this.lastAssignedVar}.send_keys ${JSON.stringify(text)}`;
+  }
+
+  codeFor_back () {
+    return `driver.back`;
+  }
+}
+
+RubyFramework.readableName = "Ruby";
+
+export default RubyFramework;
