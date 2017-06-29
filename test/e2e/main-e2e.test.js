5ad431a487fc17b05db969188d3b159ea7ddcf0e
Dan Graham <dan@saucelabs.com>
2017-06-29 14:26:33 -0700

    Refactor mocha tests to use es6 (#234)
    * Transpiles mocha e2e tests using babel
    * Refactor tests in main-e2e.test.js to use async/await, const, etc...
    * Stubbed in inspector-e2e.test.js

--- test/e2e/main-e2e.test.js
+++ test/e2e/main-e2e.test.js
@@ -1,56 +1,52 @@
-var Application = require('spectron').Application;
-var assert = require('assert');
-var os = require('os');
-var platform = os.platform();
-var path = require('path');
+import { Application } from  'spectron';
+import os from 'os';
+import path from 'path';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+const platform = os.platform();
+
+chai.should();
+chai.use(chaiAsPromised);
+
+let appPath;
+if (platform === 'linux') {
+  appPath = path.join(__dirname, '..', '..', 'release', 'linux-unpacked', 'appium-desktop');
+} else if (platform === 'darwin') {
+  appPath = path.join(__dirname, '..', '..', 'release', 'mac', 'Appium.app', 'Contents', 'MacOS', 'Appium');
+} else if (platform === 'win32') {
+  appPath = path.join(__dirname, '..', '..', 'release', 'win-ia32-unpacked', 'Appium.exe');
+}
+
+before(function () {
+  this.timeout(process.env.TRAVIS || process.env.APPVEYOR ? 10 * 60 * 1000 : 30 * 1000);
+  this.app = new Application({
+    path: appPath,
+  });
+  return this.app.start();
+});
+
+after(function () {
+  if (this.app && this.app.isRunning()) {
+    return this.app.stop();
+  }
+});
 
 describe('application launch', function () {
-  this.timeout(process.env.TRAVIS || process.env.APPVEYOR ? 10 * 60 * 1000 : 30 * 1000);
-
-  var appPath;
-  if (platform === 'linux') {
-    appPath = path.join(__dirname, '..', '..', 'release', 'linux-unpacked', 'appium-desktop');
-  } else if (platform === 'darwin') {
-    appPath = path.join(__dirname, '..', '..', 'release', 'mac', 'Appium.app', 'Contents', 'MacOS', 'Appium');
-  } else if (platform === 'win32') {
-    appPath = path.join(__dirname, '..', '..', 'release', 'win-ia32-unpacked', 'Appium.exe');
-  }
-
-  beforeEach(function () {
-    this.app = new Application({
-      path: appPath,
-    });
-    return this.app.start();
+  it('shows an initial window', async function () {
+    await this.app.client.getWindowCount().should.eventually.equal(1);
   });
 
-  afterEach(function () {
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop();
-    }
-  });
-
-  it('shows an initial window', function () {
-    return this.app.client.getWindowCount().then(function (count) {
-      assert.equal(count, 1);
-    });
-  });
-
-  it('starts the server and opens a new session window', function (done) {
+  it('starts the server and opens a new session window', async function () {
     var client = this.app.client;
-    client.waitForExist('form button.ant-btn-primary');
+    await client.waitForExist('form button.ant-btn-primary');
     var startServerBtn = client.element('form button.ant-btn-primary');
     startServerBtn.click();
-    client.waitForExist('div[class^="ServerMonitor"]').then(function () {
-      client.source().then(function (source) {
-        assert.equal(source.value.indexOf('Welcome to Appium') >= 0, true);
-        client.element('*[class*="button-container"] button').click();
-        client.pause(500).then(function () {
-          client.getWindowCount().then(function (count) {
-            assert.equal(count, 2);
-            done();
-          });
-        });
-      });
-    });
+    await client.waitForExist('div[class^="ServerMonitor"]');
+    const source = await client.source();
+    source.value.indexOf('Welcome to Appium').should.be.above(0);
+    client.element('*[class*="button-container"] button').click();
+    await client.pause(500);
+    await client.getWindowCount().should.eventually.equal(2);
   });
 });
