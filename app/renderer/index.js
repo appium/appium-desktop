59e3a5616f28d3807d49c95e9a57b2b368516779
Jonathan Lipps <jlipps@gmail.com>
2017-07-07 14:01:52 -0700

    fix recorder css and add docs for recorder

--- app/renderer/index.js
+++ app/renderer/index.js
@@ -6,6 +6,7 @@ import { syncHistoryWithStore } from 'react-router-redux';
 import routes from './routes';
 import configureStore from './store/configureStore';
 import './styles/app.global.css';
+import './styles/github-gist-theme.global.css';
 
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
