b40bc2e8bdc5b0b56454363073853786c5e987eb
dpgraham <dan@saucelabs.com>
2017-10-22 21:42:23 -0700

    Check Electron is in application folder
    * Stubbed in check if Electron is in application folder
    * Had to upgrade to 1.8.1 and make adjustments to the scripts because babel-register isn't in .bin any more

--- app/renderer/index.js
+++ app/renderer/index.js
@@ -3,17 +3,23 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import electron from 'electron';
 import routes from './routes';
 import configureStore from './store/configureStore';
 import './styles/app.global.css';
 import './styles/github-gist-theme.global.css';
 
+const {app} = electron.remote;
+const isDev = process.env.NODE_ENV === 'development';
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
 
 render(
   <Provider store={store}>
-    <Router history={history} routes={routes} />
+    {(app.isInApplicationsFolder() || (isDev && !process.env.WRONG_FOLDER)) ?
+      <Router history={history} routes={routes} /> :
+      <div>Not in applications folder</div>
+    }
   </Provider>,
   document.getElementById('root')
 );
