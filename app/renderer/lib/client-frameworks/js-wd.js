2a683bf492272486fe881cf358bfa9a69cfff205
Dan Graham <dan@saucelabs.com>
2017-12-13 14:03:50 -0800

    Fix 'wd' recorded code errors (#363)

--- app/renderer/lib/client-frameworks/js-wd.js
+++ app/renderer/lib/client-frameworks/js-wd.js
@@ -65,16 +65,16 @@ main().catch(console.log);
   }
 
   codeFor_tap (varNameIgnore, varIndexIgnore, x, y) {
-    return `await (new TouchAction(driver))
+    return `await (new wd.TouchAction(driver))
   .tap({x: ${x}, y: ${y}})
   .perform()
     `;
   }
 
   codeFor_swipe (varNameIgnore, varIndexIgnore, x1, y1, x2, y2) {
-    return `await (new TouchAction(driver))
+    return `await (new wd.TouchAction(driver))
   .press({x: ${x1}, y: ${y1}})
-  .moveTo({x: ${x2}: y: ${y2}})
+  .moveTo({x: ${x2}, y: ${y2}})
   .release()
   .perform()
     `;
