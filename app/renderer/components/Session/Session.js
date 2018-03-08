d8c4c67ccafce3e38f9fff2d2ae37bc087893a8b
Jonathan Lipps <jlipps@gmail.com>
2018-03-07 19:33:45 -0800

    mock out a unified ui using cards

--- app/renderer/components/Session/Session.js
+++ app/renderer/components/Session/Session.js
@@ -4,7 +4,7 @@ import NewSessionForm from './NewSessionForm';
 import SavedSessions from './SavedSessions';
 import AttachToSession from './AttachToSession';
 import ServerTypeTabs from './ServerTypeTabs';
-import { Tabs, Button, Spin, Icon } from 'antd';
+import { Tabs, Button, Spin, Icon, Card } from 'antd';
 import SessionStyles from './Session.css';
 
 const {TabPane} = Tabs;
@@ -26,28 +26,43 @@ export default class Session extends Component {
       requestSaveAsModal, newSession, caps, capsUUID, saveSession, isCapsDirty,
       sessionLoading, attachSessId} = this.props;
 
+    let mode = "inspect";
+
     const isAttaching = tabKey === 'attach';
 
     return <Spin spinning={!!sessionLoading}>
       <div className={SessionStyles['session-container']}>
-        <ServerTypeTabs {...this.props} />
+        <Tabs activeKey={mode} className={SessionStyles.topmostTabs}>
+          <TabPane tab='Inspect & Record Tests' key="inspect">
+          </TabPane>
+          <TabPane tab='Run a Saved Test' key="playback">
+            <div>Choose a previously-recorded test to run.</div>
+          </TabPane>
+          <TabPane tab='View Saved Tests' key="view">
+          </TabPane>
+        </Tabs>
+        <Card className={SessionStyles.sessionCard}>
+          <ServerTypeTabs {...this.props} />
+        </Card>
 
 
         {newSessionBegan && <div key={2}>
           <p>Session In Progress</p>
         </div>}
 
-        {!newSessionBegan && <Tabs activeKey={tabKey} onChange={switchTabs} className={SessionStyles.scrollingTabCont}>
-          <TabPane tab='Desired Capabilities' key='new' className={SessionStyles.scrollingTab}>
-            <NewSessionForm {...this.props} />
-          </TabPane>
-          <TabPane tab={`Saved Capability Sets (${savedSessions.length})`} key='saved' className={SessionStyles.scrollingTab} disabled={savedSessions.length === 0}>
-            <SavedSessions {...this.props} />
-          </TabPane>
-          <TabPane tab='Attach to Session...' key='attach' className={SessionStyles.scrollingTab}>
-            <AttachToSession {...this.props} />
-          </TabPane>
-        </Tabs>}
+        {!newSessionBegan && <Card className={`${SessionStyles.sessionCard} ${SessionStyles.sessionCardCaps}`}>
+          <Tabs activeKey={tabKey} onChange={switchTabs} className={SessionStyles.scrollingTabCont}>
+            <TabPane tab='Desired Capabilities' key='new' className={SessionStyles.scrollingTab}>
+              <NewSessionForm {...this.props} />
+            </TabPane>
+            <TabPane tab={`Saved Capability Sets (${savedSessions.length})`} key='saved' className={SessionStyles.scrollingTab} disabled={savedSessions.length === 0}>
+              <SavedSessions {...this.props} />
+            </TabPane>
+            <TabPane tab='Attach to Session...' key='attach' className={SessionStyles.scrollingTab}>
+              <AttachToSession {...this.props} />
+            </TabPane>
+          </Tabs>
+        </Card>}
         <div className={SessionStyles.sessionFooter}>
           <div style={{float: 'left'}}>
             <a href="#" onClick={(e) => e.preventDefault() || shell.openExternal("https://github.com/appium/appium/blob/master/docs/en/writing-running-appium/caps.md")}>
