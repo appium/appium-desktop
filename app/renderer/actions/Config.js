2a6460a00893bbc5e5fc3a872179a3a36c32695c
dpgraham <dan@saucelabs.com>
2018-07-24 10:14:13 -0700

    Add config pages

---
+++ app/renderer/actions/Config.js
@@ -0,0 +1,8 @@
+export const NAME_OF_ACTION_1 = 'NAME_OF_ACTION_1';
+export const NAME_OF_ACTION_2 = 'NAME_OF_ACTION_2';
+
+export function someAction () {
+  return async (/*dispatch, getState*/) => {
+
+  };
+}
---
+++ app/renderer/containers/ConfigPage.js
@@ -0,0 +1,14 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as ConfigActions from '../actions/Config';
+import ConfigPage from '../components/Config/Config';
+
+function mapStateToProps (state) {
+  return state.inspector;
+}
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators(ConfigActions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ConfigPage);
