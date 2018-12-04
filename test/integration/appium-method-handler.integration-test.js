09103257d79ed0d99013d19ff54cb39fd41ca0bd
Dan Graham <dan@saucelabs.com>
2018-12-04 11:28:01 -0800

    Update Mocha and Chai to latest versions (#754)
    * chore: update mocha and chai
    
    * xvfb
    
    * Add reporters
    
    * Close server when done
    
    * End E2E tests

--- test/integration/appium-method-handler.integration-test.js
+++ test/integration/appium-method-handler.integration-test.js
@@ -19,36 +19,40 @@ const DEFAULT_CAPS = {
 };
 
 describe('appiumDriverExtender', function () {
-  let driver;
+  let driver, appiumHandler, server;
 
   before(async function () {
-    await startAppiumFakeDriverServer(FAKE_DRIVER_PORT, '127.0.0.1');
-    let p = await wd.promiseChainRemote({
+    server = await startAppiumFakeDriverServer(FAKE_DRIVER_PORT, '127.0.0.1');
+    driver = await wd.promiseChainRemote({
       hostname: '127.0.0.1',
       port: FAKE_DRIVER_PORT,
     });
-    await p.init(DEFAULT_CAPS);
-    driver = new AppiumMethodHandler(p);
+    await driver.init(DEFAULT_CAPS);
+    appiumHandler = new AppiumMethodHandler(driver);
+  });
+  after(async function () {
+    await driver.quit();
+    await server.close();
   });
 
   describe('.fetchElement, .fetchElements', function () {
     it('should return empty object if selector is null', async function () {
-      const res = await driver.fetchElement('xpath', '//BadXPath');
+      const res = await appiumHandler.fetchElement('xpath', '//BadXPath');
       res.should.deep.equal({});
     });
     it('should fetchElement and cache it', async function () {
-      const {id, variableName, variableType, strategy, selector} = await driver.fetchElement('xpath', '//MockListItem');
+      const {id, variableName, variableType, strategy, selector} = await appiumHandler.fetchElement('xpath', '//MockListItem');
       id.should.exist;
       strategy.should.equal('xpath');
       selector.should.equal('//MockListItem');
       should.not.exist(variableName); // Shouldn't have a variable name until a method is performed on it
       variableType.should.equal('string');
-      driver.elementCache[id].should.exist;
-      should.not.exist(driver.elementCache[id].variableName);
-      driver.elementCache[id].variableType.should.equal('string');
+      appiumHandler.elementCache[id].should.exist;
+      should.not.exist(appiumHandler.elementCache[id].variableName);
+      appiumHandler.elementCache[id].variableType.should.equal('string');
     });
     it('should fetchElements and cache all of them', async function () {
-      const res = await driver.fetchElements('xpath', '//MockListItem');
+      const res = await appiumHandler.fetchElements('xpath', '//MockListItem');
       res.elements.length.should.be.above(0);
       res.variableName.should.equal('els1');
       res.variableType.should.equal('array');
@@ -60,18 +64,18 @@ describe('appiumDriverExtender', function () {
       res.elements[1].id.should.exist;
       res.strategy.should.equal('xpath');
       res.selector.should.equal('//MockListItem');
-      driver.elementCache[res.elements[0].id].variableName.should.equal('els1');
-      driver.elementCache[res.elements[0].id].variableType.should.equal('string');
-      driver.elementCache[res.elements[1].id].variableName.should.equal('els1');
-      driver.elementCache[res.elements[1].id].variableType.should.equal('string');
+      appiumHandler.elementCache[res.elements[0].id].variableName.should.equal('els1');
+      appiumHandler.elementCache[res.elements[0].id].variableType.should.equal('string');
+      appiumHandler.elementCache[res.elements[1].id].variableName.should.equal('els1');
+      appiumHandler.elementCache[res.elements[1].id].variableType.should.equal('string');
     });
   });
   describe('.executeElementCommand', function () {
     it('should call the click method and have the variableName, variableType, etc... returned to it with source/screenshot', async function () {
-      const {id, variableName, variableType} = await driver.fetchElement('xpath', '//MockListItem');
+      const {id, variableName, variableType} = await appiumHandler.fetchElement('xpath', '//MockListItem');
       should.not.exist(variableName); // Shouldn't have a cached variable name until a method is performed on it
       const {source, screenshot, variableName:repeatedVariableName, 
-        variableType:repeatedVariableType, id:repeatedId} = await driver.executeElementCommand(id, 'click');
+        variableType:repeatedVariableType, id:repeatedId} = await appiumHandler.executeElementCommand(id, 'click');
       repeatedVariableName.should.exist;
       variableType.should.equal(repeatedVariableType);
       id.should.equal(repeatedId);
@@ -79,11 +83,11 @@ describe('appiumDriverExtender', function () {
       screenshot.should.exist;
     });
     it('should call the click method and have the variableName, variableType, etc... returned to it with source/screenshot', async function () {
-      const {elements} = await driver.fetchElements('xpath', '//MockListItem');
+      const {elements} = await appiumHandler.fetchElements('xpath', '//MockListItem');
       for (let element of elements) {
         const {id, variableName, variableType} = element;
         const {source, screenshot, variableName:repeatedVariableName, 
-          variableType:repeatedVariableType, id:repeatedId} = await driver.executeElementCommand(id, 'click');
+          variableType:repeatedVariableType, id:repeatedId} = await appiumHandler.executeElementCommand(id, 'click');
         variableName.should.equal(repeatedVariableName);
         variableType.should.equal(repeatedVariableType);
         id.should.equal(repeatedId);
@@ -94,10 +98,10 @@ describe('appiumDriverExtender', function () {
   });
   describe('.executeMethod', function () {
     it('should call "setGeolocation" method and get result plus source and screenshot', async function () {
-      const res = await driver.executeMethod('setGeoLocation', [100, 200]);
+      const res = await appiumHandler.executeMethod('setGeoLocation', [100, 200]);
       res.screenshot.should.exist;
       res.source.should.exist;
-      const getGeoLocationRes = await driver.executeMethod('getGeoLocation');
+      const getGeoLocationRes = await appiumHandler.executeMethod('getGeoLocation');
       getGeoLocationRes.res.latitude.should.equal(100);
       getGeoLocationRes.res.longitude.should.equal(200);
 
