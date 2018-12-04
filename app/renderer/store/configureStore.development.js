a7e30489498c1a1ebae2cc3947eb9763eb79fb48
dpgraham <dpgraham.85@gmail.com>
2018-12-04 11:57:01 -0800

    Update redux-logger

--- app/renderer/store/configureStore.development.js
+++ app/renderer/store/configureStore.development.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import { hashHistory } from 'react-router';
 import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers';
