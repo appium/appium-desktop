29bb9848d9a0288720b5f62b766ad94ee1b62f41
dpgraham <dpgraham.85@gmail.com>
2018-12-05 11:32:26 -0800

    Fix

---
+++ app/main.js
@@ -0,0 +1,98 @@
+import { app, BrowserWindow, Menu } from 'electron';
+import { initializeIpc } from './main/appium';
+import { setSavedEnv } from './main/helpers';
+import menuTemplates from './main/menus';
+import shellEnv from 'shell-env';
+import fixPath from 'fix-path';
+
+let menu;
+let template;
+let mainWindow = null;
+const isDev = process.env.NODE_ENV === 'development';
+
+if (isDev) {
+  require('electron-debug')(); // eslint-disable-line global-require
+}
+
+if (!isDev) {
+  // if we're running from the app package, we won't have access to env vars
+  // normally loaded in a shell, so work around with the shell-env module
+  const decoratedEnv = shellEnv.sync();
+  process.env = {...process.env, ...decoratedEnv};
+
+  // and we need to do the same thing with PATH
+  fixPath();
+}
+setSavedEnv();
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
+
+
+const installExtensions = async () => {
+  if (isDev) {
+    const installer = require('electron-devtools-installer'); // eslint-disable-line global-require
+    const extensions = [
+      'REACT_DEVELOPER_TOOLS',
+      'REDUX_DEVTOOLS'
+    ];
+    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+    for (const name of extensions) {
+      try {
+        await installer.default(installer[name], forceDownload);
+      } catch (e) {} // eslint-disable-line
+    }
+  }
+};
+
+app.on('ready', async () => {
+  await installExtensions();
+
+  mainWindow = new BrowserWindow({
+    show: false,
+    width: isDev ? 1200 : 650,
+    height: 600,
+    minWidth: 650,
+    minHeight: 600,
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    mainWindow.show();
+    mainWindow.focus();
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  if (isDev) {
+    mainWindow.openDevTools();
+  }
+
+  mainWindow.webContents.on('context-menu', (e, props) => {
+    const {x, y} = props;
+
+    Menu.buildFromTemplate([{
+      label: 'Inspect element',
+      click () {
+        mainWindow.inspectElement(x, y);
+      }
+    }]).popup(mainWindow);
+  });
+
+  if (process.platform === 'darwin') {
+    template = await menuTemplates.mac(mainWindow);
+    menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+  } else {
+    template = await menuTemplates.other(mainWindow);
+    menu = Menu.buildFromTemplate(template);
+    mainWindow.setMenu(menu);
+  }
+
+  initializeIpc(mainWindow);
+});
+
