ba8e3653723f4b92b39d1de67b141a5f0ba53e0f
dpgraham <dpgraham.85@gmail.com>
2018-12-19 15:18:28 -0800

    Fix

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = (api) => {
   return {
     plugins: ["@babel/plugin-transform-runtime", "transform-class-properties"],
     presets: [
-      require("@babel/env"),
+      require("@babel/preset-env"),
       {
         targets: { node: 10 },
         useBuiltIns: 'usage'
