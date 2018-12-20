5d7bac9f55d9208cdca247f3e726c9fb231f4b46
dpgraham <dpgraham.85@gmail.com>
2018-12-20 15:53:38 -0800

    Fix

--- app/main.js
+++ app/main.js
@@ -59,7 +59,7 @@ app.on('ready', async () => {
     minHeight: 600,
   });
 
-  mainWindow.loadURL(`file://${__dirname}/../dist/renderer/index.html`);
+  mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
 
   mainWindow.webContents.on('did-finish-load', () => {
     mainWindow.show();
