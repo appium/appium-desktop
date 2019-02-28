728ccaaf95d29e8d93a2e3c0465ecec2f56b449a
dpgraham <dpgraham.85@gmail.com>
2019-02-28 13:49:56 -0800

    Add spacing between commands

--- app/renderer/components/Inspector/Actions.js
+++ app/renderer/components/Inspector/Actions.js
@@ -30,28 +30,34 @@ export default class Actions extends Component {
       t, selectActionGroup, selectSubActionGroup, selectedActionGroup, selectedSubActionGroup, pendingAction,
       cancelPendingAction, setActionArg } = this.props;
     return [
-      <Col span={24}>
-        <Select style={{width: '100%'}} onChange={(actionGroupName) => selectActionGroup(actionGroupName)} placeholder="Select Action Group">
-          { _.keys(actionDefinitions).map((actionGroup) => <Option key={actionGroup}>{t(actionGroup)}</Option>) }
-        </Select>
-      </Col>,
-      selectedActionGroup && <Col span={24}>
-        <Select style={{width: '100%'}} onChange={(actionGroupName) => selectSubActionGroup(actionGroupName)} placeholder="Select Sub Group">
-          { _.keys(actionDefinitions[selectedActionGroup]).map((actionGroup) => <Option key={actionGroup}>{t(actionGroup)}</Option>) }
-        </Select>
-      </Col>,
+      <Row gutter={16} style={{marginBottom: '8px'}}>
+        <Col span={24}>
+          <Select style={{width: '100%'}} onChange={(actionGroupName) => selectActionGroup(actionGroupName)} placeholder="Select Action Group">
+            { _.keys(actionDefinitions).map((actionGroup) => <Option key={actionGroup}>{t(actionGroup)}</Option>) }
+          </Select>
+        </Col>
+      </Row>,
+      selectedActionGroup && <Row>
+        <Col span={24}>
+          <Select style={{width: '100%'}} onChange={(actionGroupName) => selectSubActionGroup(actionGroupName)} placeholder="Select Sub Group">
+            { _.keys(actionDefinitions[selectedActionGroup]).map((actionGroup) => <Option key={actionGroup}>{t(actionGroup)}</Option>) }
+          </Select>
+        </Col>
+      </Row>,
       selectedSubActionGroup && _.toPairs(actionDefinitions[selectedActionGroup][selectedSubActionGroup]).map(([actionName, action]) => <Col span={8}>
-        <Button style={{width: '100%'}} onClick={() => this.startPerformingAction(actionName, action) }>{actionName}</Button>
+        <div style={{padding: '16px'}}>
+          <Button style={{width: '100%'}} onClick={() => this.startPerformingAction(actionName, action) }>{actionName}</Button>
+        </div>
       </Col>),
       !!pendingAction && <Modal
         title={pendingAction.actionName}
-        visible={!!pendingAction}
         okText={t('Execute Action')}
+        visible={!!pendingAction}
         onOk={() => this.executeCommand()}
         onCancel={() => cancelPendingAction()}>
         {
-          !_.isEmpty(pendingAction.action.args) && _.map(pendingAction.action.args, ([argName, argType], index) => <Row key={index}>
-            <Col span={24}>
+          !_.isEmpty(pendingAction.action.args) && _.map(pendingAction.action.args, ([argName, argType], index) => <Row key={index} gutter={16}>
+            <Col span={24} style={{padding: '8px 8px 0 0'}}>
               {argType === NUMBER && <Input type="number" value={pendingAction.args[index]} addonBefore={t(argName)} onChange={(e) => setActionArg(index, _.toNumber(e.target.value))} />}
               {argType === BOOLEAN && <Input addonBefore={t(argName)} />}
               {argType === STRING && <Input addonBefore={t(argName)} onChange={(e) => setActionArg(index, e.target.value)}/>}
