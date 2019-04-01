b55dcb7d5ff02d07dd4d8bb64ea799b70d58eaff
Dan Graham <dan@saucelabs.com>
2019-03-31 21:00:32 -0700

    Revert "Add way to select overlapped behind views of Screenshot component (#813)" (#917)
    This reverts commit d929c8eb0b84ee810c32ff31e9aaf962aae8ad3e.

--- app/renderer/components/Inspector/HighlighterRect.js
+++ app/renderer/components/Inspector/HighlighterRect.js
@@ -9,7 +9,7 @@ import { parseCoordinates } from './shared';
 export default class HighlighterRect extends Component {
 
   render () {
-    const {selectedElement = {}, selectHoveredElement, unselectHoveredElement, hoveredElement = {}, element,
+    const {selectedElement = {}, selectHoveredElement, unselectHoveredElement, hoveredElement = {}, selectElement, unselectElement, element,
            zIndex, scaleRatio, xOffset, elLocation, elSize} = this.props;
     const {path: hoveredPath} = hoveredElement;
     const {path: selectedPath} = selectedElement;
@@ -45,6 +45,7 @@ export default class HighlighterRect extends Component {
     return <div className={highlighterClasses.join(' ').trim()}
       onMouseOver={() => selectHoveredElement(key)}
       onMouseOut={unselectHoveredElement}
+      onClick={() => key === selectedPath ? unselectElement() : selectElement(key)}
       key={key}
       style={{zIndex, left: (left || 0), top: (top || 0), width: (width || 0), height: (height || 0)}}>
       <div></div>
--- app/renderer/components/Inspector/Screenshot.js
+++ app/renderer/components/Inspector/Screenshot.js
@@ -4,7 +4,7 @@ import HighlighterRects from './HighlighterRects';
 import { Spin, Tooltip } from 'antd';
 import B from 'bluebird';
 import styles from './Inspector.css';
-import { parseCoordinates, SCREENSHOT_INTERACTION_MODE } from './shared';
+import { SCREENSHOT_INTERACTION_MODE } from './shared';
 import { withTranslation } from '../../util';
 
 const {TAP, SELECT, SWIPE} = SCREENSHOT_INTERACTION_MODE;
@@ -37,98 +37,12 @@ class Screenshot extends Component {
     });
   }
 
-  /**
-   * Find, switch proper element for element-selection
-   * @param {*} x User clicked point X
-   * @param {*} y User clicked point Y
-   */
-  clickScreenshotElement (x, y) {
-    const {selectElement, unselectElement, source, selectedElement} = this.props;
-    const {scaleRatio} = this.state;
-    const {left: highlighterXOffset, top: highlighterYOffset} = this.containerEl.getBoundingClientRect();
-    let thisSelectedElementKey = null;
-    let shouldSelectNext = false;
-    let newSelect = true;
-    let isCoordinatesInElement = (element, x, y) => {
-      if (element == null) {
-        return false;
-      }
-      const {x1, y1, x2, y2} = parseCoordinates(element);
-      const left = x1 / scaleRatio + highlighterXOffset;
-      const top = y1 / scaleRatio + highlighterYOffset;
-      const width = (x2 - x1) / scaleRatio;
-      const height = (y2 - y1) / scaleRatio;
-      return x > left && x < (left + width) && y > top && y < (top + height);
-    };
-
-    if (selectedElement != null && isCoordinatesInElement(selectedElement, x, y)) {
-      newSelect = false;
-    }
-
-    /**
-     * Parsing tree node, find elements corresponding by user click.
-     * Corresponding element by user click is 'Candidate element'.
-     * Candidate element should be normally most child node containing click point.
-     * But candidate element also can be parents node that has child node of not containg click point.
-     * In case that parents node is wider then children.
-     * Consequently, candidate element can be several.
-     *
-     * There is 'Selection Order' among candidate elements.
-     * Whenever user clicks same point, selected node is changed by selection order.
-     *
-     * This function set following two values.
-     * 1. shouldSelectNext - True if it is last of Selection Order. By this value, next candidate node know it's my turn. Also, By this value, next operation is decided to unselection.
-     * 2. thisSelectedElementKey - Element path of candidate element which should be selected at this turn.
-     *
-     * @param {*} element each of screen shot element
-     */
-    let recursive = (element) => {
-      if (!element) {
-        return false;
-      }
-      let noChildElementIsInRect = true;
-      for (let childElement of element.children) {
-        if (recursive(childElement)) {
-          noChildElementIsInRect = false;
-        }
-      }
-
-      if (isCoordinatesInElement(element, x, y)) {
-        //If this is most child element among rects containing click point
-        if (element.children.length === 0 || noChildElementIsInRect) {
-          if ((selectedElement == null || newSelect) && thisSelectedElementKey == null) {
-            thisSelectedElementKey = element.path;
-          } else {
-            if (selectedElement != null && selectedElement.path === element.path) {
-              shouldSelectNext = true;
-            } else if (shouldSelectNext) {
-              thisSelectedElementKey = element.path;
-              shouldSelectNext = false;
-            }
-          }
-        }
-        return true;
-      }
-      return false;
-    };
-    recursive(source);
-    if (shouldSelectNext || thisSelectedElementKey == null) {
-      unselectElement();
-    } else {
-      selectElement(thisSelectedElementKey);
-    }
-  }
-
-  async handleScreenshotClick (e) {
+  async handleScreenshotClick () {
     const {screenshotInteractionMode, applyClientMethod,
            swipeStart, swipeEnd, setSwipeStart, setSwipeEnd} = this.props;
     const {x, y} = this.state;
-    /**
-     * This is way to select overlapped behind views of Screenshot component
-     */
-    if (screenshotInteractionMode === SELECT) {
-      this.clickScreenshotElement(e.clientX, e.clientY);
-    } else if (screenshotInteractionMode === TAP) {
+
+    if (screenshotInteractionMode === TAP) {
       applyClientMethod({
         methodName: 'tap',
         args: [x, y],
