7783322e978c6e14e8710e6ce2a7b5d7a6d6bd68
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-07-30 02:45:44 +0900

    docs: add pt-bz translation (#1056)

--- app/configs/app.config.js
+++ app/configs/app.config.js
@@ -1,7 +1,6 @@
 const config = {
   platform: process.platform,
-  languages: ['en', 'de', 'ru', 'ja', 'zh-CN', 'ko', 'hi', 'kn', 'ml-IN', 'pa-IN', 'te'],
-  fallbackLng: 'en',
+  languages: ['en', 'de', 'ru', 'ja', 'zh-CN', 'ko', 'hi', 'kn', 'ml-IN', 'pa-IN', 'te', 'pt-BR'],
   namespace: 'translation',
 };
 
