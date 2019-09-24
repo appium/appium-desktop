0c837ce1ca60b28b23a780d80ab2de54316e7524
dpgraham <dan@saucelabs.com>
2019-09-24 07:56:52 -0700

    Revert "Save capabilities when click outside component (#1039)"
    This reverts commit b11fcf719fe87360573a94f3aa3137a984a6f5a1.

--- app/renderer/components/Session/FormattedCaps.js
+++ app/renderer/components/Session/FormattedCaps.js
@@ -3,9 +3,8 @@ import formatJSON from 'format-json';
 import SessionCSS from './Session.css';
 import { Card, Button, Alert, Tooltip } from 'antd';
 import { getCapsObject } from '../../actions/Session.js';
-import ClickOutside from 'react-click-outside-component';
 
-export default class FormattedCaps extends Component {
+export default class NewSessionForm extends Component {
 
   getFormattedJSON (caps) {
     return formatJSON.plain(getCapsObject(caps));
@@ -16,26 +15,24 @@ export default class FormattedCaps extends Component {
            isValidCapsJson, invalidCapsJsonReason, t} = this.props;
     return caps && <div className={SessionCSS.formattedCapsCont}>
       <Card title={title || 'JSON Representation'} className={SessionCSS.formattedCaps}>
-        <ClickOutside onClickOutside={saveRawDesiredCaps}>
-          <div className={SessionCSS.capsEditorControls}>
-            {isEditingDesiredCaps && <Tooltip title={t('Cancel')}>
-              <Button onClick={abortDesiredCapsEditor} icon='close' className={SessionCSS.capsEditorButton} />
-            </Tooltip> }
-            {isEditingDesiredCaps && <Tooltip title={t('Save')}>
-              <Button onClick={saveRawDesiredCaps} icon='save' className={SessionCSS.capsEditorButton} />
-            </Tooltip>}
-            {!isEditingDesiredCaps && <Tooltip title={t('Edit Raw JSON')} placement="topRight" >
-              <Button onClick={startDesiredCapsEditor} icon='edit' />
-            </Tooltip> }
-          </div>
-          {isEditingDesiredCaps && <div>
-            <textarea rows='9' onChange={(e) => setRawDesiredCaps(e.target.value)} value={rawDesiredCaps} className={SessionCSS.capsEditor} />
-            {!isValidCapsJson && <Alert message={invalidCapsJsonReason} type="error" />}
-          </div>}
-          {!isEditingDesiredCaps && <div>
-            <pre>{this.getFormattedJSON(caps)}</pre>
-          </div>}
-        </ClickOutside>
+        <div className={SessionCSS.capsEditorControls}>
+          {isEditingDesiredCaps && <Tooltip title={t('Cancel')}>
+            <Button onClick={abortDesiredCapsEditor} icon='close' className={SessionCSS.capsEditorButton} />
+          </Tooltip> }
+          {isEditingDesiredCaps && <Tooltip title={t('Save')}>
+            <Button onClick={saveRawDesiredCaps} icon='save' className={SessionCSS.capsEditorButton} />
+          </Tooltip>}
+          {!isEditingDesiredCaps && <Tooltip title={t('Edit Raw JSON')} placement="topRight" >
+            <Button onClick={startDesiredCapsEditor} icon='edit' />
+          </Tooltip> }
+        </div>
+        {isEditingDesiredCaps && <div>
+          <textarea rows='9' onChange={(e) => setRawDesiredCaps(e.target.value)} value={rawDesiredCaps} className={SessionCSS.capsEditor} />
+          {!isValidCapsJson && <Alert message={invalidCapsJsonReason} type="error" />}
+        </div>}
+        {!isEditingDesiredCaps && <div>
+          <pre>{this.getFormattedJSON(caps)}</pre>
+        </div>}
       </Card>
     </div>;
   }
