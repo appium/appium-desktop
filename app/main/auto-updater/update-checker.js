31a2583dd4b953b4054844f114ef61ea8a8c94f9
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-10-19 19:54:27 +0900

    ignore autoupload if local is greater than github

--- app/main/auto-updater/update-checker.js
+++ app/main/auto-updater/update-checker.js
@@ -1,10 +1,11 @@
 import request from 'request-promise';
 import { getFeedUrl } from './config';
+import semver from 'semver';
 
 export async function checkUpdate (currentVersion) {
   try {
     const res = await request.get(getFeedUrl(currentVersion));
-    if (res) {
+    if (res && semver.lt(currentVersion, res.name)) {
       return JSON.parse(res);
     }
   } catch (ign) { }
