0b227eb4070db591a4145fef01a7376191eb05f2
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-10-20 10:46:30 +0900

    add a comment about the response

--- app/main/auto-updater/update-checker.js
+++ app/main/auto-updater/update-checker.js
@@ -4,6 +4,11 @@ import semver from 'semver';
 
 export async function checkUpdate (currentVersion) {
   try {
+    // The response is like (macOS):
+    // {  "name":"v1.15.0-1",
+    //    "notes":"* Bump up Appium to v1.15.0",
+    //    "pub_date":"2019-10-04T04:40:37Z",
+    //    "url":"https://github.com/appium/appium-desktop/releases/download/v1.15.0-1/Appium-1.15.0-1-mac.zip"}
     const res = await request.get(getFeedUrl(currentVersion));
     if (res) {
       const j = JSON.parse(res);
