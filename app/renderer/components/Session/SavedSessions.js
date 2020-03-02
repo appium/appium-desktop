54f3b3fa26edbad5c6821180d44e8f6743f93b70
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-03-02 17:31:21 +0900

    fix missing rename

--- app/renderer/components/Session/SavedSessions.js
+++ app/renderer/components/Session/SavedSessions.js
@@ -4,7 +4,7 @@ import { remote } from 'electron';
 import { Button, Row, Col, Table } from 'antd';
 import FormattedCaps from './FormattedCaps';
 import SessionCSS from './Session.css';
-import { HEIGHT_OF_SERVICE_CONFIG_AREA } from './Session.js';
+import { HEIGHT_OF_SESSION_CONFIG_AREA } from './Layout';
 
 export default class SavedSessions extends Component {
 
@@ -82,7 +82,7 @@ export default class SavedSessions extends Component {
       });
     }
 
-    const windowSizeHeight = remote.getCurrentWindow().getSize()[1] - HEIGHT_OF_SERVICE_CONFIG_AREA;
+    const windowSizeHeight = remote.getCurrentWindow().getSize()[1] - HEIGHT_OF_SESSION_CONFIG_AREA;
     return (<Row gutter={20} className={SessionCSS['saved-sessions']}>
       <Col span={12}>
         <Table
