23ebafbe6124e4d1e40ad67a73d5bb20acbe2f30
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-08-16 08:58:15 +0900

    fix: get filePaths (#1521)
    * fix: get filePaths
    
    * tweak
    
    * return value already inputted in cancel
    
    * tweak
    
    * rename
    
    * simplify

--- app/renderer/components/Session/CapabilityControl.js
+++ app/renderer/components/Session/CapabilityControl.js
@@ -4,27 +4,30 @@ import SessionStyles from './Session.css';
 import { remote } from 'electron';
 import { FileOutlined } from '@ant-design/icons';
 import { INPUT } from '../AntdTypes';
+import _ from 'lodash';
+import log from 'electron-log';
 
 const {dialog} = remote;
 
-
 export default class NewSessionForm extends Component {
 
-  getLocalFilePath (success) {
-    dialog.showOpenDialog((filepath) => {
-      if (filepath) {
-        success(filepath);
+  async getLocalFilePath () {
+    try {
+      const {canceled, filePaths} = await dialog.showOpenDialog({properties: ['openFile']});
+      if (!canceled && !_.isEmpty(filePaths)) {
+        return filePaths[0];
       }
-    });
-    this.handleSetType = this.handleSetType.bind(this);
+    } catch (e) {
+      log.error(e);
+    }
   }
 
   render () {
     const {cap, onSetCapabilityParam, isEditingDesiredCaps, id, t} = this.props;
 
-    const buttonAfter = <FileOutlined
+    const buttonAfter = (currentFilePath) => <FileOutlined
       className={SessionStyles['filepath-button']}
-      onClick={() => this.getLocalFilePath((filepath) => onSetCapabilityParam(filepath[0]))} />;
+      onClick={async () => {onSetCapabilityParam(await this.getLocalFilePath() || currentFilePath);}} />;
 
     switch (cap.type) {
       case 'text': return <Input disabled={isEditingDesiredCaps} id={id} placeholder={t('Value')} value={cap.value} onChange={(e) => onSetCapabilityParam(e.target.value)} />;
@@ -37,7 +40,7 @@ export default class NewSessionForm extends Component {
         return <Input disabled={isEditingDesiredCaps} id={id} type={INPUT.TEXTAREA} rows={4} placeholder={t('Value')} value={cap.value}
           onChange={(e) => onSetCapabilityParam(e.target.value)} />;
       case 'file': return <div className={SessionStyles.fileControlWrapper}>
-        <Input disabled={isEditingDesiredCaps} id={id} placeholder={t('Value')} value={cap.value} addonAfter={buttonAfter} />
+        <Input disabled={isEditingDesiredCaps} id={id} placeholder={t('Value')} value={cap.value} addonAfter={buttonAfter(cap.value)} />
       </div>;
 
       default:
