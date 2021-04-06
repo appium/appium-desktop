53ab40e61e91022905250cd01064a806be7141b1
Jonathan Lipps <jlipps@gmail.com>
2021-04-06 11:11:52 -0700

    refactor: pull shared and inspector code out of this repo and bring server back to top level

--- inspector/app/renderer/actions/index.js
+++
@@ -1,9 +0,0 @@
-import * as inspectorActions from './Inspector';
-import * as sessionActions from './Session';
-import * as updaterActions from '../../../../shared/actions/Updater';
-
-export default {
-  ...inspectorActions,
-  ...sessionActions,
-  ...updaterActions,
-};
--- shared/actions/Updater.js
+++
@@ -1,7 +0,0 @@
-export const SET_UPDATE_STATE = 'SET_UPDATE_STATE';
-
-export function setUpdateState (updateState) {
-  return (dispatch) => {
-    dispatch({type: SET_UPDATE_STATE, updateState});
-  };
-}
