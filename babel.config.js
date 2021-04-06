1845ab677b4062cf4e931fd4a1cfba2cf79ec91c
Jonathan Lipps <jlipps@gmail.com>
2021-04-06 12:11:47 -0700

    refactor: bring in gui-libs dep and use it to generate babel config

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,2 @@
-/* eslint global-require: off */
-module.exports = (api) => {
-  api.cache(true);
-  return {
-    plugins: ['@babel/plugin-transform-runtime', 'transform-class-properties'],
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: { electron: require('./inspector/node_modules/electron/package.json').version },
-          useBuiltIns: 'usage',
-          corejs: 2,
-        }
-      ],
-      ['@babel/preset-react'],
-    ],
-  };
-};
+const makeBabelConfig = require('@appium/gui-libs/babel-config-maker');
+module.exports = makeBabelConfig(require('./node_modules/electron/package.json').version);
