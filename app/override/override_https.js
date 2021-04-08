062d3827ab3e41e2528ffc1129b5ae1f4d1797d6
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2021-04-08 09:36:03 -0700

    chore: tweak UA for compatibility

--- app/override/override_https.js
+++ app/override/override_https.js
@@ -5,7 +5,7 @@ export const overrideHttpsUserAgent = () => {
         originalHttpsRequest = https.request;
 
   https.request = function (options, callback) {
-    options.headers['User-Agent'] = `${wdPackageJson.name}/${wdPackageJson.version} ${packageJson.name}/${packageJson.version}`;
+    options.headers['User-Agent'] = `admc/${wdPackageJson.name}/${wdPackageJson.version} ${packageJson.name}/${packageJson.version}`;
 
     return originalHttpsRequest(options, function (res) {
       if (callback) {
